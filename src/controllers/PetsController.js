87e6104dec9a75e267eeb279a9ae7da15c345d12
Anne Hunt <acathunt@gmail.com>
2024-04-09 14:29:15 -0600

    sketched out controller basics and starting service

---
+++ src/controllers/PetsController.js
@@ -0,0 +1,20 @@
+import { petsService } from "../services/PetService.js";
+import BaseController from "../utils/BaseController.js";
+
+
+export class PetsController extends BaseController {
+    constructor() {
+        super('api/pets')
+        this.router.get('', this.getPets)
+    }
+
+    async getPets(request, response, next) {
+        try {
+            const pets = await petsService.getPets()
+            response.send(pets)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+}
